408b6c7f3a082a2ac69c9faf6ffff4b525eb8e24
Ozsbourn <mibaranin@gmail.com>
2024-01-17 12:57:11 +0500

    rf & zustand

---
+++ RF-DAG1.x/src/App.tsx
@@ -0,0 +1,94 @@
+import React, { useCallback, useState, useRef } from 'react';
+
+import ReactFlow, {
+		 ReactFlowProvider,
+		 Controls,
+		 Background,
+		 MiniMap
+}
+from 'reactflow';
+import 'reactflow/dist/style.css';
+
+import { shallow } from 'zustand/shallow';
+import useStore		from './store/store';
+
+import nodeTypes	 from './initialData/nodeTypes'; 
+
+import Sidebar from './components/DnDSidebar';
+
+
+
+const selector = (state) => ({	nodes: state.nodes,
+	edges: state.edges,
+	onNodesChange: state.onNodesChange,
+	onEdgesChange: state.onEdgesChange,
+	onConnect: state.onConnect,
+    appendNode: state.appendNode,
+});
+
+
+function App() {
+	// const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useStore(selector, shallow); // RFC: deprecated #1937
+	const reactFlowWrapper = useRef(null);
+    const { nodes, edges, onNodesChange, onEdgesChange, onConnect, appendNode } = useStore(selector);
+    const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
+    const onDragOver = useCallback((event) => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
+    }, []);
+    const onDrop = useCallback((event) => {
+        event.preventDefault();
+
+        const type = event.dataTransfer.getData('application/reactflow');
+        if (typeof type === 'undefined' || !type) {
+            return;
+        }
+
+        const position = reactFlowInstance!.screenToFlowPosition({
+            x: event.clientX,
+            y: event.clientY,
+        });
+        const newNode = {
+            type,
+            position,
+            data: {
+                label: `${type} node`,
+            }
+        }
+
+        appendNode(newNode);
+    }, [reactFlowInstance],);
+
+
+
+	return (
+        <div className='dndflow'>
+            <ReactFlowProvider>
+                <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+                    <ReactFlow
+                        nodes={nodes}
+                        edges={edges}
+                        onNodesChange={onNodesChange}
+                        onEdgesChange={onEdgesChange}
+                        onConnect={onConnect}
+                        nodeTypes={nodeTypes}
+
+                        onInit={setReactFlowInstance}
+                        onDrop={onDrop}
+                        onDragOver={onDragOver}
+
+                        fitView
+                    >
+                        <Background variant="dots" gap={12} size={1} />
+                        <Controls />
+                        <MiniMap pannable zoomable/>
+                    </ReactFlow>
+                </div>
+                <Sidebar />
+            </ReactFlowProvider>
+        </div>
+	);
+}
+
+export default App;
---
+++ RF-DAG1.x/src/store/store.ts
@@ -0,0 +1,76 @@
+// import { create } from 'zustand'; // RFC: deprecate #1937
+import { createWithEqualityFn as create } from 'zustand/traditional';
+import { shallow }                        from 'zustand/shallow';
+
+import {
+	Connection,
+	Edge,
+	EdgeChange,
+	Node,
+	NodeChange,
+    addNodes,
+	addEdge,
+	OnNodesChange,
+	OnEdgesChange,
+	OnConnect,
+	applyNodeChanges,
+	applyEdgeChanges,
+} from 'reactflow';
+
+import initialNodes from '../initialData/nodes';
+import initialEdges from '../initialData/edges';
+
+
+
+type RFState = {
+    currId: number;
+    getId:  getId;
+
+	nodes:  Node[];
+	edges:  Edge[];
+
+	onNodesChange: OnNodesChange;
+	onEdgesChange: OnEdgesChange;
+	
+    onConnect: OnConnect;
+
+    appendNode: appendNode;
+};
+
+
+const useStore = create<RFState>((set, get, shallow) => ({
+    currId: 0,
+    getNewId: () => {
+        return `dndnode_${get().currId++}`;
+    },
+
+	nodes: initialNodes,
+	edges: initialEdges,
+
+	onNodesChange: (changes: NodeChange[]) => {
+    	set({
+    		nodes: applyNodeChanges(changes, get().nodes),
+    	});
+	},
+
+	onEdgesChange: (changes: EdgeChange[]) => {
+    	set({
+    		edges: applyEdgeChanges(changes, get().edges),
+    	});
+	},
+
+	onConnect: (connection: Connection) => {
+    	set({
+    		edges: addEdge(connection, get().edges),
+    	});
+	},
+
+    appendNode: (node: Node) => {
+        node.id = get().getNewId();
+        set({
+            nodes: get().nodes.concat(node),
+        });
+    }
+}));
+
+export default useStore;
