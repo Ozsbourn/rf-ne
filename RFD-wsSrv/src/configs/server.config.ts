4f9bc0f3671e79e42547456cac68d7b56a1b4014
Ozsbourn <mibaranin@gmail.com>
2024-02-09 18:14:06 +0500

    delete ws server for collab

--- RFD-wsSrv/src/configs/server.config.ts
+++
@@ -1,8 +0,0 @@
-const serverConfig = {
-    server_host: "localhost",
-    socket_port: 3002,
-
-    client_url: "http://localhost:5173",
-};
-
-export default serverConfig;
--- RFD-wsSrv/src/dataManager/dataTypes/connectionType.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-export interface Connection {
-  source: string | null;
-  target: string | null;
-  sourceHandle: string | null;
-  targetHandle: string | null;
-}
